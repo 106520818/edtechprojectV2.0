5bd5d0e8d6bda683526902008d193d8225af04e8
Oliver Wisbey <106520818@student.swin.edu.au>
2026-05-27 09:29:16 +1000

    I added and modified the files to complete the job page upto the required standard

---
+++ pages/jobs.php
@@ -0,0 +1,142 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="description" content="The jobs page for BookSmart Digital">
+    <meta name="keywords" content="jobs, edtech, listings">
+    <meta name="author" content="Oliver Wisbey, Roman Young">
+
+    <title>Our Jobs</title>
+
+    <link rel="stylesheet" type="text/css" href="../styles/jobs_layout.css">
+    <link rel="stylesheet" type="text/css" href="../styles/styles.css">
+</head>
+
+<body>
+
+<?php
+require_once("settings.php");
+
+$query = "SELECT * FROM jobs";
+$result = mysqli_query($conn, $query);
+?>
+
+<a href="../index.html">
+    <img id="book_smart_logo"
+         src="../Images/BookSmart_Digital_logo_CAF0F8_background.png"
+         alt="BookSmart Digital Company Logo"
+         title="To home page">
+</a>
+
+<nav>
+    <ul>
+        <li><a href="../index.html">Homepage</a></li>
+        <li><a href="apply.html">Applications</a></li>
+        <li><a href="jobs.php">Jobs</a></li>
+        <li><a href="about.html">About</a></li>
+    </ul>
+</nav>
+
+<hr style="height: 3px; border: none; background-color: black;">
+
+<h1>BookSmart Digital</h1>
+
+<aside class="requirements-box">
+    <h2>Requirements</h2>
+    <p>
+        The requirements of our positions can be negotiated but it is preferred
+        that you meet our specifications before applying.
+    </p>
+</aside>
+
+<div class="jobs-container">
+
+<?php
+if (mysqli_num_rows($result) > 0) {
+
+    while ($row = mysqli_fetch_assoc($result)) {
+
+        $responsibilities = explode(",", $row['responsibilities']);
+        $expectations = explode(",", $row['expectations']);
+        $requirements = explode(",", $row['requirements']);
+?>
+
+<section class="job-box">
+
+    <h2><?php echo $row['title']; ?></h2>
+
+    <aside class="job-code">
+        <?php echo $row['job_code']; ?>
+    </aside>
+
+    <section class="job-description">
+        <p><?php echo $row['description']; ?></p>
+    </section>
+
+    <section class="job-salary">
+        <h3>Salary</h3>
+        <p>$<?php echo number_format($row['salary']); ?> per year</p>
+
+        <h3>Reporting Line</h3>
+        <p><?php echo $row['reporting_line']; ?></p>
+    </section>
+
+    <section class="job-workflow">
+
+        <h4>Your Responsibilities</h4>
+        <ul>
+            <?php
+            foreach ($responsibilities as $item) {
+                echo "<li>$item</li>";
+            }
+            ?>
+        </ul>
+
+        <h4>Expectations</h4>
+        <ol>
+            <?php
+            foreach ($expectations as $item) {
+                echo "<li>$item</li>";
+            }
+            ?>
+        </ol>
+
+    </section>
+
+    <section class="job-requirements">
+
+        <h3>Requirements</h3>
+
+        <ul>
+            <?php
+            foreach ($requirements as $item) {
+                echo "<li>$item</li>";
+            }
+            ?>
+        </ul>
+
+    </section>
+
+</section>
+
+<?php
+    }
+} else {
+    echo "<p>No jobs currently available.</p>";
+}
+
+mysqli_close($conn);
+?>
+
+</div>
+
+<footer>
+    <a href="mailto:info@edtech.com">Email Link</a>
+    <a href="https://connortaylor623.atlassian.net/jira/software/projects/GGEP/summary" target="_blank">Jira Link</a>
+    <a href="https://github.com/106520313/EdTechProject" target="_blank">Github Link</a>
+    <a href="https://106520313.github.io/EdTechProject/index.html" target="_blank">Published page</a>
+</footer>
+
+</body>
+</html>
---
+++ sql/edtechproject_db.sql
@@ -0,0 +1,74 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 25, 2026 at 06:33 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `edtechproject_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `jobs`
+--
+
+CREATE TABLE `jobs` (
+  `job_id` int(11) NOT NULL,
+  `title` varchar(100) NOT NULL,
+  `job_code` varchar(20) NOT NULL,
+  `description` text NOT NULL,
+  `responsibilities` text NOT NULL,
+  `expectations` text NOT NULL,
+  `salary` int(11) NOT NULL,
+  `reporting_line` varchar(255) NOT NULL,
+  `requirements` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `jobs`
+--
+
+INSERT INTO `jobs` (`job_id`, `title`, `job_code`, `description`, `responsibilities`, `expectations`, `salary`, `reporting_line`, `requirements`) VALUES
+(1, 'Junior Developer Position', 'Jdev1', 'As a Junior Developer you will work across both front-end (HTML) and back-end (CSS) code an ideal position for furthering your experience in the industry early on, with room to expand into further responsibilities', 'Assist in the design, development and testing of our web applications,Debug and troubleshoot application performance,Build and integrate back-end systems for expansion', 'Contribute to technical documentation,Continuously build and improve your technical skills', 60000, 'You will be expected to report to the senior developers of your team', 'Prior experience with HTML5 coding,Basic understanding of industry/web standards,Strong communication and collaboration skills'),
+(2, 'Senior Developer Position', 'Sdev1', 'You will help lead a team to build a platform, lead designing and coding fit to the best practices of modern standards', 'Design and develop our platform to above industry standards,Lead your development team through creation of the platform,Drive expansion and integration of the application to requirements', 'Skilled at working to build alignment,Drive decision making,Transparent communication', 138000, 'You will be expected to report to the project head', 'At least 7 years of in industry HTML5 coding,Advanced understanding of industry/web standards,Strong communication and collaboration skills'),
+(3,'UX/UI Designer Position', 'UXD1', 'As a UX/UI Designer you will create intuitive and visually engaging interfaces for our educational platform while collaborating closely with developers and project managers.', 'Design user-friendly web interfaces,Create wireframes and mockups for new features,Collaborate with developers to improve usability', 'Maintain consistency across platform designs,Communicate design decisions clearly,Continuously improve user experience knowledge', 85000, 'You will report directly to the lead product designer', 'Experience with Figma or Adobe XD,Strong understanding of UX principles,Ability to work collaboratively in teams');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `jobs`
+--
+ALTER TABLE `jobs`
+  ADD PRIMARY KEY (`job_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `jobs`
+--
+ALTER TABLE `jobs`
+  MODIFY `job_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
