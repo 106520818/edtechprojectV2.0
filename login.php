c5f008c6d8237e868af654c83cb5bf3782d7a316
Connor-Taylor623 <106428271@student.swin.edu.au>
2026-05-27 11:13:09 +1000

    login styling linked

--- login.php
+++ login.php
@@ -1,5 +1,4 @@
 <?php
-
 session_start();
 ?>
 
@@ -8,9 +7,36 @@ session_start();
 
 <head>
     <title>Manager Login</title>
+        <link rel="stylesheet" href="styles/styles.css">
 </head>
 
+
+
 <body>
+    <a href="index.php">
+        <img id=book_smart_logo src="Images/BookSmart_Digital_logo_CAF0F8_background.png"
+            alt="BookSmart Digital Company Logo" title="To home page">
+    </a>
+    <nav>
+        <ul>
+            <li><a href="../pages/index.php" target="_self">Homepage</a></li>
+            <li><a href="../pages/apply.php" target="_self">Applications</a></li>
+            <li><a href="../pages/jobs.php" target="_self">Jobs</a></li>
+            <li><a href="../pages/about.php" target="_self">About</a></li>
+            <li><a href="login.php" target="_self">Manager Login</a></li>
+
+        </ul>
+    </nav>
+
+    <form>
+        <label for="search">Search:</label>
+        <input type="text" id="search" name = "search" placeholder="jobs, about, etc.">
+        <button type="submit">Search</button>
+    </form>
+
+    <h1 style="text-align: center;">BookSmart Digital</h1>
+
+    <hr style="height: 3px; border: none; background-color: black;">
 
 <h1>HR Manager Login</h1>
 
@@ -29,6 +55,10 @@ session_start();
     <input type="submit" value="Login">
 
 </form>
+
+<?php 
+include 'IncFiles/footer.inc';
+?>
 
 </body>
 </html>
