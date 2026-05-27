80c5b78a42a252d6d1a5ba57e28701b229d187da
Oliver Wisbey <106520818@student.swin.edu.au>
2026-05-27 10:26:57 +1000

    implemented .inc files again

--- pages/jobs.php
+++ pages/jobs.php
@@ -1,120 +1,144 @@
 <!DOCTYPE html>
-<!--
-Created: 01/04/2026
-Last Edited: 13/04/2026
-Authors: Oliver Wisbey, Roman Young
-Description: ___
--->
-
 <html lang="en">
 
-<head>
-    <meta charset="uft-8">
-    <meta name="description" content="The jobs page for ...">
-    <meta name="keywords" content="jobs, edtech, listings,">
-    <meta name="author" content="Oliver Wisbey, Roman Young">
-    <title>Our Jobs</title>
-    <!-- references to external files for css -->
-    <link rel="stylesheet" type="text/css" href="../styles/jobs_layout.css">
-    <link rel="stylesheet" type="text/css" href="../styles/styles.css">
-</head>
+    <head>
+        <meta charset="utf-8">
+        <meta name="description" content="The jobs page for BookSmart Digital">
+        <meta name="keywords" content="jobs, edtech, listings">
+        <meta name="author" content="Oliver Wisbey, Roman Young">
 
-<?php
-include '../IncFiles/header.inc';
-?>
+        <title>Our Jobs</title>
 
-<body>
-    <!--heading of jobs page followed by horizontal rule to break up space-->
+        <link rel="stylesheet" type="text/css" href="../styles/jobs_layout.css">
+        <link rel="stylesheet" type="text/css" href="../styles/styles.css">
+    </head>
 
-    <aside> 
+    <body>
+
+    <?php
+    require_once("settings.php");
+
+    $search = "";
+
+    if (isset($_GET['search'])) {
+        $search = mysqli_real_escape_string($conn, $_GET['search']);
+    }
+
+    $query = "SELECT * FROM jobs 
+            WHERE title LIKE '%$search%'";
+
+    $result = mysqli_query($conn, $query);
+    ?>
+
+    <?php
+    include '../IncFiles/header.inc';
+    ?>
+
+    <aside class="requirements-box">
         <h2>Requirements</h2>
-        <p>the requirements of our positions can be negotiated but it is prefered that you meet our specifications before applying</p>
+        <p>
+            The requirements of our positions can be negotiated but it is preferred
+            that you meet our specifications before applying.
+        </p>
     </aside>
 
-    <section id="Jdev_box">
-        <h2>Junior Developer Position</h2>
-        <aside>Jdev1</aside>
-        <p id="dev_desc">As a Junior Developer you will work across both front-end (HTML) and back-end (CSS) code an ideal position for furthering your experience in the industry early on, with room to expend into further responsibilities</p>
+    <form method="get" action="jobs.php" class="search-container">
 
-        <section id="dev_sal">
-            <h3>salary</h3>
-            <p>expected salary of $60,000 per year</p>
-            <h3>Reporting line</h3>
-            <p>You will be expected to report to the senior developers of your team</p>
-        </section>
+        <input
+            type="text"
+            name="search"
+            placeholder="Search job titles..."
+            value="<?php echo htmlspecialchars($search); ?>">
 
-        <section id="dev_WF">
-            <h4>Your responsibilities in this Position</h4>
-            <ul>
-                <li>Assist in the design, develpement and testing of our web applications</li>
-                <hr>
-                <li>Debug and troubleshoot application performance</li>
-                <hr>
-                <li>Build and integrate back-end systems for expansion</li>
-            </ul>
-            <h4>Expectations in this Position</h4>
-            <ol>
-                <li>Contribute to technical documentation</li>
-                <hr>
-                <li>continuously build and improve your technical skills</li>
-            </ol>
-        </section>
+        <button type="submit">Search</button>
 
+    </form>
 
-        <section id="dev_req">
-            <h3>requirements for this position</h3>
-            <ul>
-                <li>prior experience with Html5 coding</li>
-                <li>basic understanding of industry/web standards</li>
-                <li>Strong communication and collaboration skills</li>
-            </ul>
-        </section>
-    </section>
+    <div class="jobs-container">
 
-    <section id="Sdev_box">
-        <h2>Senior Developer Position</h2>
-        <aside>Sdev1</aside>
-        <p id="dev_desc">You will help lead a team to build a platform, lead designing and coding fit to the best practises of modern standards</p>
+    <?php
+    if (mysqli_num_rows($result) > 0) {
 
-        <section id="dev_sal">
-            <h3>salary</h3>
-            <p>expected salary of $138,000 per year</p>
-            <h3>Reporting line</h3>
-            <p>You will be expected to report to the project head</p>
-        </section>
+        while ($row = mysqli_fetch_assoc($result)) {
 
-        <section id="dev_WF">
-            <h4>your responsibilities in this Position</h4>
-            <ul>
-                <li>Design and develop our platform to above industry standards</li>
-                <hr>
-                <li>Lead your developement team through creation of the platform</li>
-                <hr>
-                <li>Drive expansion and integration of the application to requirements</li>
-            </ul>
-            <h4>Expectations in this Position</h4>
-            <ol>
-                <li>Skilled at working to build alignment</li>
-                <hr>
-                <li>Drive decision making</li>
-                <hr>
-                <li>Transparent communication</li>
-            </ol>
-        </section>
-
-
-        <section id="dev_req">
-            <h3>requirements for this position</h3>
-            <ul>
-                <li>at least 7 years of in industry Html5 coding</li>
-                <li> advanced understanding of industry/web standards</li>
-                <li>Strong communication and collaboration skills</li>
-            </ul>
-        </section>
-    </section>
-    
- <?php include '../IncFiles/footer.inc';
+            $responsibilities = explode(",", $row['responsibilities']);
+            $expectations = explode(",", $row['expectations']);
+            $requirements = explode(",", $row['requirements']);
     ?>
-</body>
+
+    <section class="job-box">
+
+        <h2><?php echo $row['title']; ?></h2>
+
+        <aside class="job-code">
+            <?php echo $row['job_code']; ?>
+        </aside>
+
+        <section class="job-description">
+            <p><?php echo $row['description']; ?></p>
+        </section>
+
+        <section class="job-salary">
+            <h3>Salary</h3>
+            <p>$<?php echo number_format($row['salary']); ?> per year</p>
+
+            <h3>Reporting Line</h3>
+            <p><?php echo $row['reporting_line']; ?></p>
+        </section>
+
+        <section class="job-workflow">
+
+            <h4>Your Responsibilities</h4>
+            <ul>
+                <?php
+                foreach ($responsibilities as $item) {
+                    echo "<li>$item</li>";
+                }
+                ?>
+            </ul>
+
+            <h4>Expectations</h4>
+            <ol>
+                <?php
+                foreach ($expectations as $item) {
+                    echo "<li>$item</li>";
+                }
+                ?>
+            </ol>
+
+        </section>
+
+        <section class="job-requirements">
+
+            <h3>Requirements</h3>
+
+            <ul>
+                <?php
+                foreach ($requirements as $item) {
+                    echo "<li>$item</li>";
+                }
+                ?>
+            </ul>
+
+        </section>
+
+    </section>
+
+    <?php
+        }
+    } else {
+        echo "<p>No jobs currently available.</p>";
+    }
+
+    mysqli_close($conn);
+    ?>
+
+    </div>
+
+    <?php include '../IncFiles/footer.inc';
+        ?>
+
+    </body>
 
 </html>
+
