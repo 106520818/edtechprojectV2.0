2fbbfc30f63ace22bc2662cb86ac4ebe9a3b81ab
Oliver Wisbey <106520818@student.swin.edu.au>
2026-05-27 11:08:12 +1000

    updated database

--- sql/jobportal.sql
+++ sql/jobportal.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 25, 2026 at 06:33 AM
+-- Generation Time: May 27, 2026 at 03:07 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.0.30
 
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `edtechproject_db`
+-- Database: `jobportal`
 --
 
 -- --------------------------------------------------------
@@ -46,7 +46,19 @@ CREATE TABLE `jobs` (
 INSERT INTO `jobs` (`job_id`, `title`, `job_code`, `description`, `responsibilities`, `expectations`, `salary`, `reporting_line`, `requirements`) VALUES
 (1, 'Junior Developer Position', 'Jdev1', 'As a Junior Developer you will work across both front-end (HTML) and back-end (CSS) code an ideal position for furthering your experience in the industry early on, with room to expand into further responsibilities', 'Assist in the design, development and testing of our web applications,Debug and troubleshoot application performance,Build and integrate back-end systems for expansion', 'Contribute to technical documentation,Continuously build and improve your technical skills', 60000, 'You will be expected to report to the senior developers of your team', 'Prior experience with HTML5 coding,Basic understanding of industry/web standards,Strong communication and collaboration skills'),
 (2, 'Senior Developer Position', 'Sdev1', 'You will help lead a team to build a platform, lead designing and coding fit to the best practices of modern standards', 'Design and develop our platform to above industry standards,Lead your development team through creation of the platform,Drive expansion and integration of the application to requirements', 'Skilled at working to build alignment,Drive decision making,Transparent communication', 138000, 'You will be expected to report to the project head', 'At least 7 years of in industry HTML5 coding,Advanced understanding of industry/web standards,Strong communication and collaboration skills'),
-(3,'UX/UI Designer Position', 'UXD1', 'As a UX/UI Designer you will create intuitive and visually engaging interfaces for our educational platform while collaborating closely with developers and project managers.', 'Design user-friendly web interfaces,Create wireframes and mockups for new features,Collaborate with developers to improve usability', 'Maintain consistency across platform designs,Communicate design decisions clearly,Continuously improve user experience knowledge', 85000, 'You will report directly to the lead product designer', 'Experience with Figma or Adobe XD,Strong understanding of UX principles,Ability to work collaboratively in teams');
+(3, 'UX/UI Designer Position', 'UXD1', 'As a UX/UI Designer you will create intuitive and visually engaging interfaces for our educational platform while collaborating closely with developers and project managers.', 'Design user-friendly web interfaces,Create wireframes and mockups for new features,Collaborate with developers to improve usability', 'Maintain consistency across platform designs,Communicate design decisions clearly,Continuously improve user experience knowledge', 85000, 'You will report directly to the lead product designer', 'Experience with Figma or Adobe XD,Strong understanding of UX principles,Ability to work collaboratively in teams');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `user_id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Indexes for dumped tables
@@ -59,6 +71,12 @@ ALTER TABLE `jobs`
   ADD PRIMARY KEY (`job_id`);
 
 --
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`user_id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -66,7 +84,13 @@ ALTER TABLE `jobs`
 -- AUTO_INCREMENT for table `jobs`
 --
 ALTER TABLE `jobs`
-  MODIFY `job_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `job_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
